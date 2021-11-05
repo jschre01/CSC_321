607d862c09c5a4e17edfa18313971d5d09db00a6
jphaws <jphaws@calpoly.edu>
2021-11-04 18:54:51 -0700

    Finished task 3

--- OTP/task3.py
+++ OTP/task3.py
@@ -1,63 +1,44 @@
+from progressbar import ProgressBar
 from base64 import b64encode
 from sys import argv
 from os import urandom
 
 def main():
-    if len(argv) != 3:
-        print("Usage: task2.py <input file> <output file>")
+    __HEADER_BYTES__ = 54
+    if len(argv) != 4:
+        print("Usage: task2.py <input image> <output image ciphertext> <output image plaintext>")
         exit(1)
-    with open(argv[1], 'r') as fd1:
-        plaintext = fd1.read()
 
-        key_bytes = urandom(len(plaintext))
-        key_str = b64encode(key_bytes).decode('UTF-8')
-        key_str = key_str[0:len(plaintext)]
-        key_hex = convert_to_hex(key_str)
+    with open(argv[1], 'rb') as fd1:
+        bmp_header = fd1.read(__HEADER_BYTES__)
+        bmp_bytes = fd1.read()
 
-        print(f"Plaintext:  {plaintext}")
-        print(f"Key string: {key_str}")
-        print(f"Key Length: {len(key_str)}, Plaintext Length: {len(plaintext)}")
-        print(f"Key Hex:        {key_hex}")
+        key_bytes = urandom(len(bmp_bytes))
 
-        ciphertext_hex = xor_strings(plaintext, key_str)
-        print(f"Ciphertext hex: {ciphertext_hex}")
+        ciphertext = xor_bytes(bmp_bytes, key_bytes)
 
-        ciphertext_string = convert_from_hex(ciphertext_hex)
+        # write encrypted image
+        with open(argv[2], 'wb') as fd2:
+            fd2.write(bmp_header)
+            fd2.write(ciphertext)
 
-        print(f"Ciphertext string: {ciphertext_string}")
-        decrypted_hex_text = xor_strings(ciphertext_string, key_str)
-        print(f"Plaintext Hex: {decrypted_hex_text}")
+        # write decrypted image
+        with open(argv[3], 'wb') as fd3:
+            decrypted_bytes = xor_bytes(ciphertext, key_bytes)
+            fd3.write(bmp_header)
+            fd3.write(decrypted_bytes)
 
-        decrypted_string_text = convert_from_hex(decrypted_hex_text)
-        print(f"Plaintext: {decrypted_string_text}")
+def xor_bytes(byte_string1: bytes, byte_string2: bytes) -> bytes:
+    if len(byte_string1) != len(byte_string2):
+        raise ValueError(f"bytes are not the same length: {len(byte_string1)} and {len(byte_string2)}")
 
-        with open(argv[2], 'w') as fd2:
-            fd2.write(decrypted_string_text)
-
-# Converts character string to hex string
-def convert_to_hex(string: str) -> str:
-    hex_str = ""
-    for c in range(len(string)):
-        hex_str = hex_str + format(ord(string[c]), '02x')
-    return hex_str
-
-# Converts hex string to character string
-def convert_from_hex(hex_str: str) -> str:
-    bytes_array = bytes.fromhex(hex_str)
-    ascii_str = bytes_array.decode()
-
-    return ascii_str
-
-def xor_strings(string1: str, string2: str) -> str:
-    if len(string1) != len(string2):
-        raise ValueError(f"Strings are not the same length: {len(string1)} and {len(string2)}")
-
-    xor = ""
-    for c in range(len(string1)):
-        temp = ord(string1[c]) ^ ord(string2[c])
-        xor = xor + format(temp, '02x')
+    xor = b''
+    pbar = ProgressBar()
+    for c in pbar(range(len(byte_string1))):
+        xor += (byte_string1[c] ^ byte_string2[c]).to_bytes(1, 'big')
 
     return xor
+
 
 if __name__ == "__main__":
     main()
