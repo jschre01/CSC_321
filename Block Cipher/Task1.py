358b92aad650aa3bbe1cc3dda30e150d8ccd6c58
jphaws <jphaws@calpoly.edu>
2021-11-05 11:59:37 -0700

    Co-authored-by: jschre01 <jschre01@users.noreply.github.com>

--- Block Cipher/Task1.py
+++ Block Cipher/Task1.py
@@ -5,31 +5,58 @@ import sys
 
 def main():
     in_file = sys.argv[0]
-    f = open(in_file, 'r')
-    text = pad(f, 16)
-    ecb_key = #generate random key of ecb size
-    cbc_key = #generate random key of cbc size
-    cbc_iv = #generate random iv of iv size
+    bmp_header = None
+    if '.bmp' in in_file:
+        f = open(in_file, 'rb')
+        bmp_header = f.read(54)
+    else:
+        f = open(in_file, 'r')
 
-    f_ecb = open("ecb_encrypt.txt", "w")
-    f_cbc = open("cbc_encrypt.txt", "w")
-    ecb_cipher = AES.new(ecb_key, AES.MODE_ECB)
+    plaintext = f.read()
+
+    
+    text = pad(bytearray(plaintext.encode('utf-8')), 8)
+
+    file_ecb = open("ecb_encrypt.txt", "w")
+    file_cbc = open("cbc_encrypt.txt", "w")
+
+    ecb_encrypt(text, file_ecb, bmp_header)
+    cbc_encrypt(text, file_cbc, bmp_header)
+
+    #encrypt = AES.new(ecb_key, AES.MODE_ECB)
     ##NEED TO PAD f BEFORE DOING THIS vvv Can use Crypto.Util.Padding
-    ecb_ct = ecb_cipher.encrypt(text)
-    cbc_cipher = AES.new(cbc_key, AES.MODE_CBC, cbc_iv)
-    cbc_ct = cbc_cipher.encrypt(text)
+    #ecb_ct = ecb_cipher.encrypt(text)
+    #cbc_cipher = AES.new(cbc_key, AES.MODE_CBC, cbc_iv)
+    #cbc_ct = cbc_cipher.encrypt(text)
 
-    f_ecb.write(ecb_ct)
-    f_cbc.write(cbc_ct)
+    #f_ecb.write(ecb_ct)
+    #f_cbc.write(cbc_ct)
 
     f.close()
-    f_ecb.close()
-    f_cbc.close()
+    file_ecb.close()
+    file_cbc.close()
 
 def generate_key(num_bytes:int) -> bytes:
     return get_random_bytes(num_bytes)
 
-def ecb_encrypt():
+def cbc_encrypt(text, outfile, bmp_header):
+    if bmp_header != None:
+        outfile.write(bmp_header)
+    cbc_key = generate_key(16)
+    iv = generate_key(8)
+    encrypt = AES.new(cbc_key, AES.MODE_ECB)
+    for i in range(len(text), 8):
+        iv = encrypt.encrypt(text[i:i+8] ^ iv)
+        outfile.write(iv)
+
+def ecb_encrypt(text, outfile, bmp_header):
+    if bmp_header != None:
+        outfile.write(bmp_header)
+    ecb_key = generate_key(16)
+    encrypt = AES.new(ecb_key, AES.MODE_ECB)
+    for i in range(len(text), 8):
+        outfile.write(encrypt.encrypt(text[i:i+8]))
+    
 
 
 
