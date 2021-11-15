ab2e5096ca03d5bb35f04cf29b7817c0fd2f19d7
Jonathan Schreiber <jonny@Jonathans-MacBook-Pro-3.local>
2021-11-15 11:48:32 -0800

    Started task 2

---
+++ Block_Cipher/Task2.py
@@ -0,0 +1,14 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Task1 import pkcs, generate_key, cbc_encrypt
+
+
+def main():
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
