fa50f7b27b48458e4df739d2ab50921f70190c71
jphaws <jphaws@calpoly.edu>
2021-12-02 23:21:17 -0800

    Finished

--- Hashing and Passwords/task1.py
+++ Hashing and Passwords/task1.py
@@ -1,5 +1,6 @@
+from itertools import permutations
 from hashlib import sha256
-import itertools
+from time import time
 
 def main():
     # Print menu for task
@@ -22,25 +23,31 @@ def main():
         print(sha256hex1)
         print(sha256hex2)
     elif (option == 'c'): 
-        for i in range(1, 51):
+        for i in range(8, 51):
             generate_collisions(i)
 
+# Returns hex string SHA256 hash
 def create_sha256_hash(plaintext: str) -> str:
     hash = sha256(plaintext.encode('utf-8')).hexdigest()
     return hash
 
+# Returns the bitwise representation of SHA256 hash of specified length
 def create_sha256_bits(plaintext: str, length: int) -> str:
     hash = create_sha256_hash(plaintext)
     bits = bin(int(hash, base=16)).lstrip('0b')
     return bits[:length]
 
+# Generates a list of characters to use for collision generation
 def get_list_of_chars():
     list = []
     for i in range(48, 145):
         list.append(chr(i))
     return list
 
+# Generates collisions for hashes of specific length
 def generate_collisions(bit_len: int):
+    start_time = time()
+    input_count = 0
     # Get a list of legal characters ascii codes 1-255
     legal_chars = get_list_of_chars()
 
@@ -51,7 +58,7 @@ def generate_collisions(bit_len: int):
     for str_len in range(1, 5):
 
         # Get all permutations of legal characters of length str_length
-        perms = list(itertools.permutations(legal_chars, r=str_len))
+        perms = list(permutations(legal_chars, r=str_len))
 
         # For each permutation generated...
         # Merge the tuple into a string
@@ -59,14 +66,17 @@ def generate_collisions(bit_len: int):
         for tup in perms:
             plaintext = ''.join(tup)
             hash_bits = create_sha256_bits(plaintext, bit_len)
+            input_count += 1
 
             # If raw bits exist as a key, we have a collision
             if hash_bits in htable:
-                print(f"COLLISION at length {bit_len}: {{{hash_bits}}} : {{{plaintext.encode('utf-8').hex()}}} and {{{htable[hash_bits].encode('utf-8').hex()}}}")
+                delta = time() - start_time
+                print(f"COLLISION at {bit_len} bits: {{{hash_bits:50}}} : "
+                f"{{{plaintext.encode('utf-8').hex():8}}} and {{{htable[hash_bits].encode('utf-8').hex():8}}} "
+                f"in {delta:9} seconds with {input_count} inputs")
                 return
             else:
                 htable[hash_bits] = plaintext
-    input("No COllISIONS: Press enter to continue")
 
 if __name__ == "__main__":
     main()
